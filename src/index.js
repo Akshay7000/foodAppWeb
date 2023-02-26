c446116cab480fef1e570dd3b6a877231e9c8d49
akshay <akshay.patel@moreyeahs.co>
2023-02-27 00:20:16 +0530

    pwa

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
-import { theme } from "./theme/theme.config";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import "./index.css";
+import { store } from "./redux/store";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { theme } from "./theme/theme.config";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -22,5 +23,5 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
+serviceWorkerRegistration.register();
 reportWebVitals();
